d0c9a98231dae67b1d5784f5a43bee4f27b93242
duvan <duvanfoz@hotmail.com>
2023-02-06 11:59:16 -0500

    feat: products views and update controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show]
+  before_action :set_product, only: %i[show edit update destroy]
   def index
     @products = Product.all
   end
