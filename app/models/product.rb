b674ab8ba94b468f6a30cbb48b4269bc2444b5fd
duvan <duvanfoz@hotmail.com>
2023-02-07 15:17:31 -0500

    feat: active storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,7 @@
 #  updated_at  :datetime         not null
 #
 class Product < ApplicationRecord
+  has_one_attached :image
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
 end
