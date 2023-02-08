f51bd468c505692f0ab7735b8a9f1f59a49d2945
duvan <duvanfoz@hotmail.com>
2023-02-08 11:26:57 -0500

    feat: Category Scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,18 @@
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
 #
 class Product < ApplicationRecord
+  belongs_to :category
   has_one_attached :image
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :products
+  resources :categories, except: %i[show]
+  resources :products, path: "/"
 end
