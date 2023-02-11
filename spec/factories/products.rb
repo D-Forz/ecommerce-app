ff3a9933c0731e4a36fb65eeac6b243bf180a96d
duvan <duvanfoz@hotmail.com>
2023-02-11 07:33:32 -0500

    test: update factories

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -9,18 +9,22 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  category_id :bigint           not null
+#  user_id     :bigint           not null
 #
 # Indexes
 #
 #  index_products_on_category_id  (category_id)
+#  index_products_on_user_id      (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (user_id => users.id)
 #
 FactoryBot.define do
   factory :product do
     category
+    user
     title { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price }
