2f27f0778f581dd2f6b441dcfd32fc05ad325d2e
duvan <duvanfoz@hotmail.com>
2023-02-13 09:35:11 -0500

    test: show profile

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Users", type: :request do # rubocop:disable Metrics/BlockLength
+  describe "GET /user/:username" do
+    let(:user) { create(:user) }
+
+    context "when the user exists" do
+      it "return http status 200 and render the show template" do
+        get user_path(user.username)
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("show")
+      end
+    end
+
+    context "when the user does not exist" do
+      it "redirect to the products path" do
+        get user_path("invalid")
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(products_path)
+      end
+    end
+  end
+
   describe "GET /users/new" do
     it "return http status 200 and render the new template" do
       get new_user_path
